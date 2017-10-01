c82487165b0635ef31be5ba49ff7603897a570ed
emiliowd <eherna40@gmail.com>
2017-10-01 18:02:32 +0200

    Subiendo Proyecto

---
+++ README.md
@@ -0,0 +1,18 @@
+# platzi-form
+
+> Proyecto de práctica en vuejs, realizando un front end de un formulario de registro con un diseño muy minimalista y limpio.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template lang="pug">
+  #app
+    .container
+      .columns.is-mobile.is-centered
+        .column.is-one-third-tablet.is-three-quarters-mobile
+          section.hero
+            .hero-body
+              h1.title SIGN UP
+          .form
+            .field(v-for='value in dataLabel')
+              label.label {{ value.value}}
+              .control
+                input.input(type="text" placeholder="")
+            .field
+              label.label CARGO
+              .control
+                .select
+                  select
+                    option(v-for="job in jobs" v-bind:value="job.value") {{ job.name}}
+            .control
+              button.button SIGNUP
+            
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      dataLabel:[{
+          value: 'NOMBRE',
+      },
+      {
+          value: 'APELLIDO',
+      }],
+      jobs: [
+          {name: 'FullStack JS', value: 'fullstackJs'},
+          {name: 'FrontEnd Developer', value: 'frontend'},
+          {name: 'Backend Developer', value: 'backend'},
+      ],
+        selectedJob: 'fullstackJs'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss';
+html{
+  height: 100vh;
+  background: #41295a;
+  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #2F0743, #41295a);
+  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #2F0743, #41295a);
+  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+
+</style>
